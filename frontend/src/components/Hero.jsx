aac1f0fbf2a043fb25de76fd57e1f50bf2082d3f
Nayeem <>
2023-08-09 17:11:40 +0530

    user registration and login

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import { Container, Card, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import '../index.css'
+
+const Hero = () => {
+  return (
+    <div className=' py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+          <h1 className='text-center mb-4'><span className='small-text'> Be a part of </span>FineBonito</h1>
+          <p className='text-center mb-4'>
+            This is a boilerplate for MERN authentication that stores a JWT in
+            an HTTP-Only cookie. It also uses Redux Toolkit and the React
+            Bootstrap library
+          </p>
+          <div className='d-flex'>
+            <LinkContainer to='/login'>
+            <Button variant='primary'  className='me-3'>
+              Login
+            </Button>
+            </LinkContainer>
+            <LinkContainer to='/signup'>
+            <Button variant='secondary' >
+              Signup
+            </Button>
+            </LinkContainer>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import store from "./Store.js";
+import {Provider} from 'react-redux'
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/signup" element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+      <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  </Provider>
+);
